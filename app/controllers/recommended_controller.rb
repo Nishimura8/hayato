ca9b37a68460e7ecec36ca5dd1e9a763a81d5ba4
西村勇人 <nishimurahayato@nishimurahayatonoMacBook-Air.local>
2019-08-14 21:15:32 +0900

    投稿機能実装

--- app/controllers/recommended_controller.rb
+++ app/controllers/recommended_controller.rb
@@ -3,6 +3,19 @@ class RecommendedController < ApplicationController
         @reco = Recommended.all
     end
 
+    def new
+       @recommended = Recommended.new
+    end
+
+    def create
+    @recommended = Recommended.new(recommended_params)
+    if @recommended.save
+        redirect_to root_path
+    else
+        render :new
+    end
+    end
+
     def show
         @recommended = Recommended.find(params[:id])
         @user = @recommended.user
